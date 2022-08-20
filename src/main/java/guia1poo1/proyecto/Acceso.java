f4b4c7e4200234b2bd23d48b6c81b970d17fce30
Javier B <101611427+JBCHujmd@users.noreply.github.com>
2022-08-20 15:51:19 -0600

    Primera parte clase acceso

--- src/main/java/guia1poo1/proyecto/Acceso.java
+++ src/main/java/guia1poo1/proyecto/Acceso.java
@@ -10,4 +10,40 @@ package guia1poo1.proyecto;
  */
 public class Acceso {
     
+    //Atributos acceso
+    private String usuario = "admin";
+    private String clave = "admin";
+    private String nombreSistema = "FarmaFiorella";
+    private String bienvenida = "Bienvenido al sistema de FaramaFiorella";
+    private String mensaje = "Por favor ingrese con sus credenciales";
+    private String ingreseUsuario = "Ingresar usuario: ";
+    private String ingreseclave = "Ingresar contraseña: ";
+
+    //Constructor
+    public Acceso() {
+    }
+
+    //Método get y set
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    //Función
+    public String getClave() {
+        return clave;
+    }
+
+    //Procedimiento
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getNombreSistema() {
+        return nombreSistema;
+    }
+
 }
