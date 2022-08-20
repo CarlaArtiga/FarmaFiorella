37658f4650a4d3d765819656eda15143eef91881
carla.artiga <202000791@ujmd.edu.sv>
2022-08-20 16:02:43 -0600

    Segunda parte de clase Acceso terminada

--- src/main/java/guia1poo1/proyecto/Acceso.java
+++ src/main/java/guia1poo1/proyecto/Acceso.java
@@ -11,13 +11,13 @@ package guia1poo1.proyecto;
 public class Acceso {
     
     //Atributos acceso
-    private String usuario = "admin";
-    private String clave = "admin";
-    private String nombreSistema = "FarmaFiorella";
-    private String bienvenida = "Bienvenido al sistema de FaramaFiorella";
-    private String mensaje = "Por favor ingrese con sus credenciales";
+    private String usuario        = "admin";
+    private String clave          = "admin";
+    private String nombreSistema  = "FarmaFiorella";
+    private String bienvenida     = "Bienvenido al sistema de FaramaFiorella";
+    private String mensaje        = "Por favor ingrese con sus credenciales";
     private String ingreseUsuario = "Ingresar usuario: ";
-    private String ingreseclave = "Ingresar contraseña: ";
+    private String ingreseclave   = "Ingresar contraseña: ";
 
     //Constructor
     public Acceso() {
@@ -45,5 +45,52 @@ public class Acceso {
     public String getNombreSistema() {
         return nombreSistema;
     }
+    
+    //Nombre del sistema
+    public void setNombreSistema(String nombreSistema){
+    this.nombreSistema = nombreSistema;    
+    }
+    
+    //Devolcuion de bienvenida
+    public String getBienvenida(){
+        return bienvenida;
+    }
+    
+    //Retorno de bienvenida
+    public void setBienvenida(String Bienvenida){
+        this.bienvenida = bienvenida;
+    }
+    
+    //Retorno mensaje
+    public String getMensaje(){
+        return mensaje;
+    }
+    
+    public String getIngreseUsuario() {
+        return ingreseUsuario;
+    }
+    
+    public void setIngreseUsuario(String ingreseUsuario) {
+        this.ingreseUsuario = ingreseUsuario;
+    }
+
+    public String getIngreseclave() {
+        return ingreseclave;
+    }
+
+    public void setIngreseclave(String ingreseclave) {
+        this.ingreseclave = ingreseclave;
+    }
+
+    //Procedimiento o Funcion para imprimir el menu
+    public void bienvenidad() {
+        System.out.println(bienvenida + nombreSistema);
+        System.out.println(mensaje);
+        System.out.println(ingreseUsuario);
+        System.out.println(ingreseclave);
+    }
+    
+    
+    
 
 }
