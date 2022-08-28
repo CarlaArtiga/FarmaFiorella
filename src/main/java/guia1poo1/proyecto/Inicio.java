2fe2e0b83cb42b780a899af763fa6c3d42e34400
carla.artiga <202000791@ujmd.edu.sv>
2022-08-28 17:49:04 -0600

    Nuevos cambios

--- src/main/java/guia1poo1/proyecto/Inicio.java
+++ src/main/java/guia1poo1/proyecto/Inicio.java
@@ -15,14 +15,14 @@ public class Inicio {
     //Instanciar clase
     static Acceso ac = new Acceso();
     static Menu mn = new Menu();
-    static Producto p = new Producto();
+   
 
     public static void main(String[] args) {
         
         ac.bienvenidad();
         mn.ingresoSistema();
-        
-       p.MostrarInventario();
+       
+      
        
     }
 }
